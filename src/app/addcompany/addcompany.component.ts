d76128d5b639079dfefdd3aca57fecb0dde071e3
Johannes Roth <jrothx@icloud.com>
2025-03-30 08:53:44 +0200

    companysite added

---
+++ src/app/addcompany/addcompany.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addcompany',
+  imports: [],
+  templateUrl: './addcompany.component.html',
+  styleUrl: './addcompany.component.scss'
+})
+export class AddcompanyComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AddcompanyComponent } from './addcompany/addcompany.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:AddcompanyComponent},
+];
